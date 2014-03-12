17c86f9c7a3150a23102e48e3ebaecee03779f67
ark2812 <arnaud.cerckel@gmail.com>
2014-03-12 21:46:54 +0100

    save what I did before merge

--- matlab/angle_estimator.m
+++ matlab/angle_estimator.m
@@ -4,22 +4,23 @@ G = fft2(f);
 
 G = fftshift(G);
 rInter = log(abs(G));
-
+%rInter = fftshift(rInter);
 angle = find_angle(f, rInter, thetas);
 end
 
 function [t] = find_angle(f, rInter, thetas)
-R = radon2(rInter,thetas);
+R = radon(rInter,thetas);
 
-mid_elemt=round(size(R,1)/2);
-mid_size=floor(min(size(f))/4*sqrt(2))-1;
-R=R(mid_elemt-mid_size:mid_elemt+mid_size,:);
+% mid_elemt=round(size(R,1)/2);
+% mid_size=floor(min(size(f))/4*sqrt(2))-1;
+% R=R(mid_elemt-mid_size:mid_elemt+mid_size,:);
 
 figure
 plot(thetas,var(R));
 title('Var de la transfo de Radon');
-[size1, size2]  = size(R(5:100, 5:175))
+%[size1, size2]  = size(R(5:100, 5:175))
 [m i] = max(var(R));
 
-t = i-1; 
+t = thetas(i); 
 end
+
