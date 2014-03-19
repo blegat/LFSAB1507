9a5abf8f8cd9328630c832a8a4340da87dd91245
ark2812 <arnaud.cerckel@gmail.com>
2014-03-19 11:38:27 +0100

    ajout testBlurDeblur

--- matlab/testBlurDeblur.m
+++ matlab/testBlurDeblur.m
@@ -13,8 +13,10 @@ close all;
 %I = double(imread('SagarL25A10.jpg'));
 
 % Computer, the professional prog has difficult so ...
-I = double(imread('computerGray.jpg'));
+%I = double(imread('computerGray.jpg'));
 
+I = double(imread('AaronGray.png'));
+%I = double(imread('SeaGray.png'));
 %To be blurred
 %cameraman
 %I = double(imread('cameraman.tif'));
@@ -23,7 +25,7 @@ I = double(imread('computerGray.jpg'));
 %I = rgb2gray((imread('filleL50A20.jpg')));
 %load fille.mat;
 
-I = blur(I,50,20,2);
+%I = blur(I,50,20,1);
 %deblurred = zeros(size(I,1),size(I,2),size(I,3));
 %  for i = 1:3
 %  deblurred(:,:,i) = deblur(I(:,:,i),algo);
