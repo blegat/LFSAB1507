6e71b807e676e3118abb50237f5f7acbf5e49936
Benoît Legat <benoit.legat@gmail.com>
2014-03-19 12:00:46 +0100

    anges

--- matlab/testBlurDeblur.m
+++ matlab/testBlurDeblur.m
@@ -1,7 +1,7 @@
 function [] = testBlurDeblur(algo)
 %petite fille
-%I = load('fille.mat');
-%I = I.I;
+I = load('fille.mat');
+I = I.I;
 %save_image(deblur(blur(I,55,60,2)), 'testBlurDeblur',2);
 
 close all;
@@ -15,7 +15,7 @@ close all;
 % Computer, the professional prog has difficult so ...
 %I = double(imread('computerGray.jpg'));
 
-I = double(imread('AaronGray.png'));
+%I = double(imread('AaronGray.png'));
 %I = double(imread('SeaGray.png'));
 %To be blurred
 %cameraman
@@ -25,7 +25,7 @@ I = double(imread('AaronGray.png'));
 %I = rgb2gray((imread('filleL50A20.jpg')));
 %load fille.mat;
 
-%I = blur(I,50,20,1);
+I = blur(I,50,20,1);
 %deblurred = zeros(size(I,1),size(I,2),size(I,3));
 %  for i = 1:3
 %  deblurred(:,:,i) = deblur(I(:,:,i),algo);
